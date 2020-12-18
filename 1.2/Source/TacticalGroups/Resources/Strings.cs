7cdbe73d582263f795f554b3d0f284709e291089
Taranchuk <nfhfyxer@gmail.com>
2020-12-19 00:06:29 +0200

    update

--- 1.2/Source/TacticalGroups/Resources/Strings.cs
+++ 1.2/Source/TacticalGroups/Resources/Strings.cs
@@ -14,7 +14,7 @@ namespace TacticalGroups
 	public static class Strings
 	{
 		public static readonly string Rally = "TG.Rally".Translate();
-		public static readonly string Actions = "TG.Actions".Translate();
+		public static readonly string Work = "TG.Work".Translate();
 		public static readonly string Orders = "TG.Orders".Translate();
 		public static readonly string Manage = "TG.Manage".Translate();
 
